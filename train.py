5776541de07be74f550699f5bbe6e14d7a2521b4
RandomEnch <1048421623@qq.com>
2022-08-12 19:48:12 +0800

    empty_cache

--- train.py
+++ train.py
@@ -225,6 +225,7 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
         utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
         utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
     global_step += 1
+    torch.cuda.empty_cache()
   
   if rank == 0:
     logger.info('====> Epoch: {}'.format(epoch))
@@ -284,6 +285,7 @@ def evaluate(hps, generator, eval_loader, writer_eval):
       audio_sampling_rate=hps.data.sampling_rate
     )
     generator.train()
+    torch.cuda.empty_cache()
 
                            
 if __name__ == "__main__":
